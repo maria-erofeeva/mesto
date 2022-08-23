a78ee1aab456b31b3aab06a223ce823be27c4489
Maria Erofeeva <erofeymm@gmail.com>
2022-08-23 16:10:51 +0300

    feat: добавлен попап

--- scripts/index.js
+++ scripts/index.js
@@ -9,12 +9,53 @@ let formInputName = document.querySelector(".popup__input_type_name");
 let formInputDescription = document.querySelector(
   ".popup__input_type_description"
 );
+let addCardButton = document.querySelector(".profile__add-photo-button");
+let modalWholePage = document.querySelector(".modal");
+let modalCloseButton = document.querySelector(".modal__close-icon");
 
-function openPopup() {
-  popupWholePage.classList.add("popup_opened");
-  formInputName.value = currentName.textContent;
-  formInputDescription.value = currentDescription.textContent;
-}
+/*const initialCards = [
+  {
+    name: "Байкал",
+    link: "https://unsplash.com/photos/2VQcuE-IFDo",
+  },
+  {
+    name: "Архангельск",
+    link: "https://unsplash.com/photos/uPx334kOgm0",
+  },
+  {
+    name: "Эльбрус",
+    link: "https://unsplash.com/photos/9qsK2QHidmg",
+  },
+  {
+    name: "Калязинская колокольня",
+    link: "https://unsplash.com/photos/s8ZVDmzGwKQ",
+  },
+  {
+    name: "Карачаевск",
+    link: "https://unsplash.com/photos/5sPYYR6lG28",
+  },
+  {
+    name: "Карелия",
+    link: "https://unsplash.com/photos/nUdN80QHMpA",
+  },
+];
+
+const out = '';
+initialCards.forEach(el => {
+    out += el.name;
+    out += '<img src="' + el.link +'">';});
+document.getElementById('outCards').innerHTML = out;*/
+
+
+function openPopup() { 
+
+  popupWholePage.classList.add("popup_opened"); 
+
+  formInputName.value = currentName.textContent; 
+
+  formInputDescription.value = currentDescription.textContent; 
+
+} 
 
 popupLink.addEventListener("click", openPopup);
 
@@ -40,3 +81,19 @@ function formSubmitHandler(evt) {
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
+
+
+
+function openModal() { 
+
+  modalWholePage.classList.add("modal_opened"); 
+
+} 
+
+addCardButton.addEventListener("click", openModal);
+
+function closeModal() {
+  modalWholePage.classList.remove("modal_opened");
+}
+
+modalCloseButton.addEventListener("click", closeModal);
