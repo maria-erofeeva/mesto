2f272ecb45390ac8ca5ffd72f06201f9e2859640
Maria Erofeeva <erofeymm@gmail.com>
2022-10-29 20:06:58 +0300

    feat

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -1,12 +1,14 @@
 export class Popup {
   constructor(popupSelector) {
     this._popupSelector = popupSelector;
-    this._handleEscClose = this._handleEscClose.bind(this);
+    this._handleEscClose = this._handleEscClose(this);
   }
 
   open() {
     this._popupSelector.classList.add("popup_opened");
-    document.addEventListener("keydown", this._handleEscClose);
+    document.addEventListener("keydown", (evt) => {
+        this._handleEscClose(evt);
+    });
   }
 
   close() {
--- src/scripts/PopupWithImage.js
+++ src/scripts/PopupWithImage.js
@@ -1,12 +1,12 @@
-import { Popup } from "./Popup.js";
+import { Popup } from './Popup.js';
 
 import { popupImg, figcaptionText } from "../utils/constants.js";
 
 export class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._title = this._popupSelector.querySelector(".gallery__image");
-    this._image = this._popupSelector.querySelector(".gallery__card-title");
+    this._image = this._popupSelector.querySelector(".gallery__image");
+    this._title = this._popupSelector.querySelector(".gallery__card-title");
   }
 
   open({ name, link }) {
